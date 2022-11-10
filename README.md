42d6eddd6ae147f13656f026ed64bd29b71f94c3
Samuel Kodytek <samuel.kodytek@guided.cz>
2022-11-10 19:04:03 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ASPNetExamples
+# Set of examples for students attending ASP.Net course
+
+View in this order:
+
+1. RazorPagesExample
---
+++ RazorPagesExample/ViewModels/UserViewModel.cs
@@ -0,0 +1,8 @@
+namespace RazorPagesExample.Models;
+
+public class UserViewModel
+{
+    public string FirstName;
+    public string LastName;
+    public DateTime BirthDate;
+}
